9deef1ade54f050cb9a4cb0df253c197bf5e7a0e
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2024-05-16 11:02:17 +0900

    ⚡️[fix] ErrorMessage 응답코드 수정

--- spotify-web/src/main/java/com/example/spotifyweb/global/common/response/ErrorMessage.java
+++ spotify-web/src/main/java/com/example/spotifyweb/global/common/response/ErrorMessage.java
@@ -9,9 +9,10 @@ import org.springframework.http.HttpStatus;
 public enum ErrorMessage {
 
     //404
-    STATION_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.NOT_FOUND.value(), "ID에 해당하는 스테이션이 존재하지 않습니다."),
 
     //400
+    STATION_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.BAD_REQUEST.value(), "ID에 해당하는 스테이션이 존재하지 않습니다."),
+
     ;
 
     private final int status;
