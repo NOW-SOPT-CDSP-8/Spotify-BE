d81e795ef69eb464d073762e96d405df01fb3ee1
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-05-17 22:27:35 +0900

    ️♻️ [refactor] dto class에서 record로 변경

--- spotify-web/src/main/java/com/example/spotifyweb/api/music/dto/MusicsGetResponseDto.java
+++ spotify-web/src/main/java/com/example/spotifyweb/api/music/dto/MusicsGetResponseDto.java
@@ -2,12 +2,8 @@ package com.example.spotifyweb.api.music.dto;
 
 import com.example.spotifyweb.api.playlist.dto.PlaylistInfo;
 import java.util.List;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@Getter
-@RequiredArgsConstructor
-public class MusicsGetResponseDto {
-    private final List<PlaylistInfo> playlists;
-    private final List<MusicInfo> musics;
+public record MusicsGetResponseDto(
+        List<PlaylistInfo> playlists,
+        List<MusicInfo> musics) {
 }
