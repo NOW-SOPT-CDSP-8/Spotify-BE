ed85afef9a5641b9536d62a7626540374326525b
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-05-17 10:39:46 +0900

    ✨[feat] 스테이션 좋아요 api 구현

--- spotify-web/src/main/java/com/example/spotifyweb/global/common/response/ErrorMessage.java
+++ spotify-web/src/main/java/com/example/spotifyweb/global/common/response/ErrorMessage.java
@@ -12,8 +12,8 @@ public enum ErrorMessage {
 
     //400
     STATION_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.BAD_REQUEST.value(), "ID에 해당하는 스테이션이 존재하지 않습니다."),
-
-    ;
+    MEMBER_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.BAD_REQUEST.value(), "ID에 해당하는 멤버가 존재하지 않습니다."),
+    DUPLICATION_STATION_LIKE(HttpStatus.BAD_REQUEST.value(), "해당하는 회원이 이미 좋아요를 한 스테이션입니다.");
 
     private final int status;
     private final String message;
