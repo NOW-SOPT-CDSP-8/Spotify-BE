35fb3ce7e975d078a99e72be882565042f2414a3
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-05-17 10:22:02 +0900

    [fix] 모든 bad request를 STATION_NOT_FOUND_BY_ID_EXCEPTION로 처리하던 에러 수정 현# 아이콘	코드	     설명

--- spotify-web/src/main/java/com/example/spotifyweb/global/common/exception/GlobalExceptionHandler.java
+++ spotify-web/src/main/java/com/example/spotifyweb/global/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.example.spotifyweb.global.common.exception;
 
 import com.example.spotifyweb.global.common.response.ApiResponse;
-import com.example.spotifyweb.global.common.response.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,9 +13,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     protected ApiResponse handleNotFoundException(NotFoundException e) {
         return ApiResponse.error(
-                ErrorMessage.STATION_NOT_FOUND_BY_ID_EXCEPTION.getStatus(),
-                ErrorMessage.STATION_NOT_FOUND_BY_ID_EXCEPTION.getMessage()
+                e.getErrorMessage().getStatus(),
+                e.getErrorMessage().getMessage()
         );
     }
-
 }
